91c2e23233df60d5be7e443b3e73be4796022ccc
Kacper Kwiatkowski <kacper18k@gmail.com>
2022-12-08 16:09:18 +0100

    Product controller done.

---
+++ product/Product.UnitTests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Moq;
+using product.Controllers;
+using product.Dto;
+using product.Service;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Product.UnitTests.Controllers;
+
+//[TestFixture]
+public class ProductControllerTest
+{
+    private readonly ProductController _productController;
+    private readonly Mock<IProductService> _mockProductService = new Mock<IProductService>();
+
+    public ProductControllerTest()
+    {
+        _productController = new ProductController(_mockProductService.Object);
+    }
+
+    [Fact]
+    public async Task GetAllProducts_ShouldReturn200_WhenExecuted()
+    {
+        // Arrange
+        var productDtoList = new List<ProductDto>() { new ProductDto(){Id = Guid.NewGuid()}};
+        
+        _mockProductService
+            .Setup(x => x.GetAllProducts())
+            .ReturnsAsync(productDtoList);
+        
+        // Act
+        var result = await _productController.GetAllProducts() as ObjectResult;
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(HttpStatusCode.OK, (HttpStatusCode) result.StatusCode);
+        Assert.Equal(productDtoList, result.Value);
+        _mockProductService.Verify(x => x.GetAllProducts(), Times.Once);
+    }
+}
