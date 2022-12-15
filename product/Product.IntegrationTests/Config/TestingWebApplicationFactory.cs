ea7ec7d1b9a283da58b277f6cdb2c16d1daea28a
Kacper Kwiatkowski <kacper18k@gmail.com>
2022-12-15 12:44:56 +0100

    In memory db setted up.

---
+++ product/Product.IntegrationTests/Config/TestingWebApplicationFactory.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using product.Data;
+
+namespace Product.IntegrationTests.Config;
+
+public class TestingWebApplicationFactory<Program> : WebApplicationFactory<Program> where Program : class
+{
+    
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<ProductDbContext>));
+
+            services.Remove(descriptor);
+            services.AddDbContext<ProductDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryDbForTesting");
+            });
+
+            var sp = services.BuildServiceProvider();
+            
+            using (var scope = sp.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<ProductDbContext>();
+                var logger = scopedServices
+                    .GetRequiredService<ILogger<TestingWebApplicationFactory<Program>>>();
+
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
+        });
+    }
+}
--- product/Product.IntegrationTests/Controllers/ProductControllerIntegrationTest.cs
+++ product/Product.IntegrationTests/Controllers/ProductControllerIntegrationTest.cs
@@ -1,20 +1,92 @@
-﻿using System.Net;
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using product.Dto;
+using Product.IntegrationTests.Config;
 using Xunit;
 
 namespace Product.IntegrationTests.Controllers;
 
-public class ProductControllerIntegrationTest
+public class ProductControllerIntegrationTest : IClassFixture<TestingWebApplicationFactory<Program>>
 {
-    [Fact]
-    public async Task GetUser_ReturnsUsers()
-    {
-        await using var app = new WebApplicationFactory<Program>();
-        using var client = app.CreateClient();
+    private readonly HttpClient _client;
 
-        var response = client.GetAsync("/product");
+    public ProductControllerIntegrationTest(
+        TestingWebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient(new WebApplicationFactoryClientOptions());
+    }
+
+    [Fact]
+    public async Task Test00()
+    {
         
-        Assert.True(response.Result.StatusCode == HttpStatusCode.OK);
+        // Arrange
+        var boothToCreate = new CreateBoothDto()
+        {
+            Title = "TestTitle",
+            Description = "TestDescription"
+        };
+    
+        // Act
+        var createBoothResponse = await _client.PostAsync("/booth", new StringContent(JsonConvert.SerializeObject(boothToCreate), Encoding.Default, "application/json"));
+        var getBoothsResponse = await _client.GetAsync("/booth");
+        var createBoothResult = createBoothResponse.Content.ReadAsStringAsync().Result;
+        var getBoothsResult = getBoothsResponse.Content.ReadAsStringAsync().Result;
+    
+        var rawBoothId = getBoothsResult.Split("\"");
+        Guid boothId = new Guid(rawBoothId[3]);
+               
+        var productToCreate = new CreateProductDto()
+        {
+            Title = "TestTitle",
+            Description = "TestDescription",
+            BoothId = boothId
+        };
+        
+        var createProductResponse = await _client.PostAsync("/product", new StringContent(JsonConvert.SerializeObject(productToCreate), Encoding.Default, "application/json"));
+        var getProductResponse = await _client.GetAsync("/product");
+        var createProductResult = createProductResponse.Content.ReadAsStringAsync().Result;
+        var getProductResult = getProductResponse.Content.ReadAsStringAsync().Result;
+    
+        // Assert
+        Assert.True(getBoothsResult.Contains(boothToCreate.Title));
+        Assert.True(getProductResult.Contains(boothToCreate.Description));
+    }
+
+    [Fact]
+    public async Task Test01_PostUser_CreatesUser()
+    {
+        // Arrange
+        CreateProductDto productDto = new CreateProductDto()
+        {
+            // BoothId = fixture._productDbContext.Booth.First().Id,
+            Title = "TestTitle",
+            Description = "TestDescription"
+        };
+        
+        // Act
+        var response = await _client.PostAsync("/product", new StringContent(JsonConvert.SerializeObject(productDto), Encoding.Default, "application/json"));
+    
+        // Assert
+        Assert.True(response.StatusCode == HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async Task Test02_GetAllProducts_ReturnsAllProducts()
+    {
+        // Arrange
+    
+        // Act
+        var response = await _client.GetAsync("/product");
+        
+        // Assert
+        Assert.True(response.StatusCode == HttpStatusCode.OK);
+        Assert.Contains("TestTitle", response.Content.ReadAsStringAsync().Result);
     }
 }
